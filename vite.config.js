727b70be191c9c0de69e6c067afc2aca3fede5cc
mistletoe235 <92664458+mistletoe235@users.noreply.github.com>
2025-01-15 22:48:59 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/academic-project-page-template-vue',
+  base:'/openfly_',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
